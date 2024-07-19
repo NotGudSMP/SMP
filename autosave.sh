b3834598da9ed10930a5e55b4a307927b72c193b
Phnganh32 <152988467+Phnganh32@users.noreply.github.com>
2024-07-19 08:22:12 +0700

    Update autosave.sh
    now have hour:minute

--- autosave.sh
+++ autosave.sh
@@ -1,5 +1,5 @@
 while [ 1 ]; do
-   git add -A && git commit -m "Autosave at $(date -d "+7 hours" '+%A, %B %d, %Y UTC+7')";
+   git add -A && git commit -m "Autosave at $(date -d "+7 hours" '"%H:%M %A, %B %d, %Y UTC+7')";
    git push -u
    sleep 600;
 done
